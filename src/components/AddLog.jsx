2a2504e50e1fca34f2a98f7a6322e14dc1f90ad7
sivani-r-anil <sivaniranil28@gmail.com>
2026-07-14 15:11:22 +0530

    NavigationBar and some styles added

--- src/components/AddLog.jsx
+++ src/components/AddLog.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import NavigationBar from './NavigationBar'
 
 const AddLog = () => {
 
@@ -49,7 +50,7 @@ const AddLog = () => {
 
     return (
         <div>
-
+            <NavigationBar />
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
